33dbad2bae62497113235f560f9b558c660ab43d
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-07-09 02:38:56 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Link from "next/link";
 import PageLayout from "@/Components/PageLayout";
+import Head from "next/head";
 
 const MyComponent = () => {
   return (
-    <PageLayout>
-      <Link href="CustomersPage">Go to the Customer Page</Link>
-    </PageLayout>
+    <>
+      <Head>
+        <title>Customers Details App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <PageLayout>
+        <Link href="CustomersPage">Go to the Customer Page</Link>
+      </PageLayout>
+    </>
   );
 };
 
