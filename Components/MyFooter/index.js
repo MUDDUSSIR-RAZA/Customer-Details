0b2c34f8fa233d3a86e5caa6342a71e8d31d21b8
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-07-09 02:39:11 +0500

    Update index.js

--- Components/MyFooter/index.js
+++ Components/MyFooter/index.js
@@ -12,6 +12,8 @@ const MyFooter = () => {
           bottom: 0,
           width: "100%",
           padding: "2px",
+          background: "#001529",
+          color: "white",
         }}
       >
         <h1> MR DEVELOPER</h1>
